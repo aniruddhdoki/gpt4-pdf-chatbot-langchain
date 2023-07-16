4f4da8d5d82a192604967434f8211547a01c154c
Aniruddh Doki <doki.aniruddh@gmail.com>
2023-07-16 15:18:07 -0400

    updates
    - added sign in and sign out with supabase
    - updated ui to use mantine
    todo
    - add chat history recording

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,50 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { useState } from 'react';
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
+
   return (
     <>
-      <main className={inter.variable}>
-        <Component {...pageProps} />
-      </main>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Head>
+          <title>aidfinder</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: 'dark',
+          }}
+        >
+          <main className={inter.variable}>
+            <Component {...pageProps} />
+          </main>
+        </MantineProvider>
+      </SessionContextProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,111 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
+import {
+  AppShell,
+  Avatar,
+  Code,
+  Container,
+  Divider,
+  Group,
+  Navbar,
+  Stack,
+  Text,
+  Title,
+  createStyles,
+  getStylesRef,
+  rem,
+} from '@mantine/core';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+const useStyles = createStyles((theme) => ({
+  // md padding on bottom, 1.5 md margin on bottom, border bottom
+  header: {
+    paddingBottom: theme.spacing.md,
+    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+  },
+
+  //
+  footer: {
+    paddingTop: theme.spacing.md,
+    marginTop: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+  },
+
+  // styling for links in navbar
+  link: {
+    ...theme.fn.focusStyles(),
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    fontSize: theme.fontSizes.sm,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[7],
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      },
+    },
+  },
+
+  //  styling for link icons
+  linkIcon: {
+    ref: getStylesRef('icon'),
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[2]
+        : theme.colors.gray[6],
+    marginRight: theme.spacing.sm,
+  },
+
+  // styling for links when active
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({
+        variant: 'light',
+        color: theme.primaryColor,
+      }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+        .color,
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+          .color,
+      },
+    },
+  },
+
+  // message styling
+  message: {
+    paddingLeft: rem(54),
+    paddingTop: theme.spacing.sm,
+  },
+}));
+
+// navbar items
+const data = [{ link: '/', label: 'chat' }];
 
 export default function Home() {
+  const { classes, cx } = useStyles();
+
+  const [active, setActive] = useState('chat');
   const [query, setQuery] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
@@ -25,21 +128,49 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi! Get started by asking a question about medicaid. Currently only supporting NC medicaid, more benefits to come :)',
+        message:
+          'Hi! Get started by asking a question. Currently supporting NC medicaid, and local resources.',
         type: 'apiMessage',
       },
     ],
     history: [],
   });
+  const [supaData, setSupaData] = useState();
 
   const { messages, history } = messageState;
-
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
   const messageListRef = useRef<HTMLDivElement>(null);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     textAreaRef.current?.focus();
   }, []);
+
+  useEffect(() => {
+    async function loadData() {
+      const { data } = await supabaseClient.from('messages')?.select('*');
+      setSupaData(data);
+    }
+
+    if (user) loadData();
+  }, [user]);
+
+  const links = data.map((item) => (
+    <a
+      className={cx(classes.link, {
+        [classes.linkActive]: item.label === active,
+      })}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <span>{item.label}</span>
+    </a>
+  ));
 
   //handle form submission
   async function handleSubmit(e: any) {
@@ -122,7 +253,277 @@ export default function Home() {
 
   return (
     <>
-      <Layout>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar height={'100%'} width={{ sm: '25%' }} p="md">
+            <Navbar.Section grow>
+              <Group className={classes.header} position="apart">
+                <span>aidfinder</span>
+                <Code>v0.0.2</Code>
+              </Group>
+              {links}
+            </Navbar.Section>
+            <Navbar.Section className={classes.footer}>
+              {user ? (
+                <a
+                  href="#"
+                  className={classes.link}
+                  onClick={() => supabaseClient.auth.signOut()}
+                >
+                  <span>Sign Out</span>
+                </a>
+              ) : (
+                <Auth
+                  redirectTo="http://localhost:3000"
+                  appearance={{ theme: ThemeSupa }}
+                  supabaseClient={supabaseClient}
+                  providers={[]}
+                  socialLayout="horizontal"
+                />
+              )}
+            </Navbar.Section>
+          </Navbar>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Container fluid>
+          <Title order={1}>welcome to aidfinder!</Title>
+          <Title order={4}>
+            i'm your guy for anything related to government benefits, so ask
+            away!
+          </Title>
+          {/* <Title order={6}>
+            *currently only supporting NC medicaid, more to come :)
+          </Title> */}
+          <Divider />
+          <Stack
+            justify="flex-start"
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+              paddingTop: rem(10),
+            })}
+            ref={messageListRef}
+          >
+            {messages.map((message, index) => {
+              let icon;
+              let className;
+              if (message.type === 'apiMessage') {
+                icon = (
+                  <Image
+                    key={index}
+                    src="/bot-image.png"
+                    alt="AI"
+                    width="40"
+                    height="40"
+                    className={styles.boticon}
+                    priority
+                  />
+                );
+                className = styles.apimessage;
+              } else {
+                icon = (
+                  <Image
+                    key={index}
+                    src="/usericon.png"
+                    alt="Me"
+                    width="30"
+                    height="30"
+                    className={styles.usericon}
+                    priority
+                  />
+                );
+                // the latest message sent by the user will be animated while waiting for a response
+                className =
+                  loading && index === messages.length - 1
+                    ? styles.usermessage
+                    : styles.usermessage;
+              }
+              return (
+                <div
+                  key={`chatMessage${index}`}
+                  className={cx(styles.message, className)}
+                >
+                  <Stack>
+                    <Text className={classes.message} size="sm">
+                      <Title order={6}>
+                        {message.type === 'apiMessage' ? 'aidfinder' : 'user'}
+                      </Title>
+                      {message.message}
+                    </Text>
+                    {message.sourceDocs && (
+                      <div className="p-5" key={`sourceDocsAccordion-${index}`}>
+                        <Accordion
+                          type="single"
+                          collapsible
+                          className="flex-col"
+                        >
+                          {message.sourceDocs.map((doc, index) => (
+                            <div key={`messageSourceDocs-${index}`}>
+                              <AccordionItem value={`item-${index}`}>
+                                <AccordionTrigger>
+                                  <h3>Source {index + 1}</h3>
+                                </AccordionTrigger>
+                                <AccordionContent>
+                                  <ReactMarkdown linkTarget="_blank">
+                                    {doc.pageContent}
+                                  </ReactMarkdown>
+                                  <p className="mt-2">
+                                    <b>Source:</b> {doc.metadata.source}
+                                  </p>
+                                </AccordionContent>
+                              </AccordionItem>
+                            </div>
+                          ))}
+                        </Accordion>
+                      </div>
+                    )}
+                  </Stack>
+                </div>
+              );
+            })}
+            {/*<div ref={messageListRef} className={styles.messagelist}>
+                {messages.map((message, index) => {
+                  let icon;
+                  let className;
+                  if (message.type === 'apiMessage') {
+                    icon = (
+                      <Image
+                        key={index}
+                        src="/bot-image.png"
+                        alt="AI"
+                        width="40"
+                        height="40"
+                        className={styles.boticon}
+                        priority
+                      />
+                    );
+                    className = styles.apimessage;
+                  } else {
+                    icon = (
+                      <Image
+                        key={index}
+                        src="/usericon.png"
+                        alt="Me"
+                        width="30"
+                        height="30"
+                        className={styles.usericon}
+                        priority
+                      />
+                    );
+                    // The latest message sent by the user will be animated while waiting for a response
+                    className =
+                      loading && index === messages.length - 1
+                        ? styles.usermessagewaiting
+                        : styles.usermessage;
+                  }
+                  return (
+                    <>
+                      <div key={`chatMessage-${index}`} className={className}>
+                        {icon}
+                        <div className={styles.markdownanswer}>
+                          <ReactMarkdown linkTarget="_blank">
+                            {message.message}
+                          </ReactMarkdown>
+                        </div>
+                      </div>
+                      {message.sourceDocs && (
+                        <div
+                          className="p-5"
+                          key={`sourceDocsAccordion-${index}`}
+                        >
+                          <Accordion
+                            type="single"
+                            collapsible
+                            className="flex-col"
+                          >
+                            {message.sourceDocs.map((doc, index) => (
+                              <div key={`messageSourceDocs-${index}`}>
+                                <AccordionItem value={`item-${index}`}>
+                                  <AccordionTrigger>
+                                    <h3>Source {index + 1}</h3>
+                                  </AccordionTrigger>
+                                  <AccordionContent>
+                                    <ReactMarkdown linkTarget="_blank">
+                                      {doc.pageContent}
+                                    </ReactMarkdown>
+                                    <p className="mt-2">
+                                      <b>Source:</b> {doc.metadata.source}
+                                    </p>
+                                  </AccordionContent>
+                                </AccordionItem>
+                              </div>
+                            ))}
+                          </Accordion>
+                        </div>
+                      )}
+                    </>
+                  );
+                })}
+              </div>*/}
+            <div className={styles.center}>
+              <div className={styles.cloudform}>
+                <form onSubmit={handleSubmit}>
+                  <textarea
+                    disabled={loading}
+                    onKeyDown={handleEnter}
+                    ref={textAreaRef}
+                    autoFocus={false}
+                    rows={1}
+                    maxLength={512}
+                    id="userInput"
+                    name="userInput"
+                    placeholder={
+                      loading
+                        ? 'Waiting for response...'
+                        : 'My client is on SSI, what could they be eligible for...'
+                    }
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className={styles.textarea}
+                  />
+                  <button
+                    type="submit"
+                    disabled={loading}
+                    className={styles.generatebutton}
+                  >
+                    {loading ? (
+                      <div className={styles.loadingwheel}>
+                        <LoadingDots color="#000" />
+                      </div>
+                    ) : (
+                      // Send icon SVG in input field
+                      <svg
+                        viewBox="0 0 20 20"
+                        className={styles.svgicon}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </form>
+              </div>
+            </div>
+            {error && (
+              <div className="border border-red-400 rounded-md p-4">
+                <p className="text-red-500">{error}</p>
+              </div>
+            )}
+          </Stack>
+        </Container>
+
+        {/* <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
             AidFinder
@@ -262,7 +663,8 @@ export default function Home() {
         </div>
         <footer className="m-auto p-4">
         </footer>
-      </Layout>
+      </Layout> */}
+      </AppShell>
     </>
   );
 }
